433ecf8d7331df219f738acd076f671bf4905672
aorongsi <47778792+aorongsi@users.noreply.github.com>
2022-03-29 21:17:37 +0800

    Add files via upload

---
+++ MFCCalculatorDlg.h
@@ -0,0 +1,48 @@
+﻿
+// MFCCalculatorDlg.h: 头文件
+//
+
+#pragma once
+
+//运算符
+enum Calculator_Flag {
+
+
+
+};
+
+// CMFCCalculatorDlg 对话框
+class CMFCCalculatorDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCCalculatorDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCCALCULATOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+private :
+	CString Str1;
+	CString Str2;
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton9();
+};
