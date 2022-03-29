6d6d1bf963f14c9f76537b0112d6bdb88dc8d0d4
aorongsi <47778792+aorongsi@users.noreply.github.com>
2022-03-29 21:27:53 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,50 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCCalculator.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCALCULATOR_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT_InPut                  1000
+#define IDC_EDIT2                       1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1007
+#define IDC_BUTTON_DIVI                 1008
+#define IDC_BUTTON7                     1009
+#define IDC_BUTTON8                     1010
+#define IDC_BUTTON9                     1011
+#define IDC_BUTTON_MULT1                1012
+#define IDC_BUTTON6                     1013
+#define IDC_BUTTON1                     1014
+#define IDC_BUTTON2                     1015
+#define IDC_BUTTON_SUB                  1016
+#define IDC_BUTTON3                     1017
+#define IDC_BUTTON_DOT                  1018
+#define IDC_BUTTON0                     1019
+#define IDC_BUTTON_SUM                  1020
+#define IDC_BUTTON_EQU                  1021
+#define IDC_BUTTON_arctanx              1022
+#define IDC_BUTTON_arcsinx              1023
+#define IDC_BUTTON_cosx                 1024
+#define IDC_BUTTON_sinx                 1025
+#define IDC_BUTTON_Clear_All            1028
+#define IDC_BUTTON_Back                 1029
+#define IDC_BUTTON_Clear                1030
+#define IDC_EDIT3                       1035
+#define IDC_EDIT4                       1036
+#define IDC_EDIT5                       1037
+#define IDC_EDIT6                       1038
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
