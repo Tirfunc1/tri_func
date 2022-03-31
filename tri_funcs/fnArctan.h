bd9db503009e8537c72b900b0f4a9e1081f9535a
pwy11 <80302342+pwy11@users.noreply.github.com>
2022-03-31 19:40:15 +0800

    Add files via upload

--- tri_funcs/fnArctan.h
+++ tri_funcs/fnArctan.h
@@ -1,26 +1,29 @@
-/*
- * @Description: 
- * @Author: Longjing Xia
- * @FilePath: \tri_func\fnArctan.h
- * @Date: 2022-03-27 15:06:41
- * @LastEditTime: 2022-03-27 16:34:20
- */
-
 #pragma once
 
-double fnArctan (double x) {
+const double pi = 3.1415926;
+
+double fnArctan(double x){
     double sqr = x * x;
-	double e = x > 0 ? x : -x;
-    int flag = x > 0 ? 1 : -1;
-	double r = 0;
-	int i = 1;
-	while (e / i > 1e-15) 
-	{
-		double f = e / i;
-		r = (i % 4 == 1)? r+f: r-f;
-		e = e * sqr;
-		i += 2;
-	}
-	return r * flag;
+    double e = x;
+    double r = 0;
+    int i = 1;
+    while (fabs(e / i) > 1e-15){
+        double f = e / i;
+        r = (i % 4 == 1)? r + f : r - f;
+        e *= sqr;
+        i += 2;
+    }
+    return r;
 }
-	
+
+double arctan(double x){
+    if (x >= -1 && x <= 1)
+        return fnArctan(x);
+    else{
+        if (x > 0)
+            return (pi/2 - fnArctan(1 / x));
+        else
+            return (-pi/2 - fnArctan(1 / x));
+   }
+}
+
